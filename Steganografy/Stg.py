62c016aa04ad78afd1e6b9b2c805e29416711c72
ezhovaid <39653291+ezhovaid@users.noreply.github.com>
2018-05-27 22:20:12 +0300

    Add files via upload

---
+++ Steganografy/Stg.py
@@ -0,0 +1,29 @@
+class ff():
+	try:
+		namefile=input("File name: ")
+		with open(namefile, 'ab') as file:
+			text=input("write the text: ")
+			file.write(text.encode("utf-8"))
+	except FileNotFoundError:
+		print("[x] File: '"+str(namefile)+"' is not defined!")
+		raise SystemExit
+	else:
+		print ("[x] File: "+str(namefile)+" successfully overwritten!")
+class fff():
+	try:
+		namefile=input("File name: ")
+		with open(namefile, "rb") as r:
+			byte = r.read(1)
+			k=0
+			while byte:
+				try:
+					byte = r.read(1).decode("utf-8")
+				except:
+					continue
+				print(byte,end="")
+				k+=1
+	except FileNotFoundError:
+		print("\n[x] File: '"+str(namefile)+"' is not defined!")
+		raise SystemExit
+	else:
+		print("\n[+] Number of bytes in the '"+str(namefile)+"': "+str(k))
